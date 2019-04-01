48720f0c49493fe0b0a88198a3be77241e89a365
Yannick Marcon <yannick.marcon@obiba.org>
2019-04-01 10:07:34 +0200

    Code clean

--- R/setupCNSIMTest.R
+++ R/setupCNSIMTest.R
@@ -1,16 +1,16 @@
 #' Setup a test environment based on the CNSIM simulated datasets
 #'
-#' Load the CNSIM datasets and the corresponding loadingdata object, instanciate a new \link{DSLiteServer}
-#' hosting these datasets, verifies that the required DataSHIELD server-side packages are installed.
+#' Load the CNSIM datasets, the corresponding login data object, instanciate a new \link{DSLiteServer}
+#' hosting these datasets and verify that the required DataSHIELD server-side packages are installed.
 #'
-#' @param packages DataSHIELD server-side packages that must be installed locally so that the \link{DSLiteServer}
-#' can execute the DataSHIELD operations.
+#' @param packages DataSHIELD server-side packages which local installation must be verified so that the \link{DSLiteServer}
+#' can auto-configure itself and can execute the DataSHIELD operations. Default is none.
 #' @param env The environment where DataSHIELD objects should be looked for: the \link{DSLiteServer} and
 #' the DSIConnection objects. Default is the Global environment.
 #' @return The login data for the \link{datashield.login} function.
 #' @family setup functions
 #' @export
-setupCNSIMTest <- function(packages, env = globalenv()) {
+setupCNSIMTest <- function(packages = c(), env = globalenv()) {
   setupDSLiteServer(packages, c("CNSIM1", "CNSIM2", "CNSIM3"),
                     "logindata.dslite.cnsim", "DSLite", "dslite.server", env)
 }
--- R/setupDSLiteServer.R
+++ R/setupDSLiteServer.R
@@ -4,18 +4,20 @@
 #' hosting these datasets, verifies that the required DataSHIELD server-side packages are installed.
 #' All the data structures are loaded by \link{data} which supports various formats (see data() documentation).
 #'
-#' @param packages DataSHIELD server-side packages that must be installed locally so that the \link{DSLiteServer}
-#' can execute the DataSHIELD operations.
+#' @param packages DataSHIELD server-side packages which local installation must be verified so that the \link{DSLiteServer}
+#' can auto-configure itself and can execute the DataSHIELD operations. Default is none.
 #' @param datasets Names of the datasets to be loaded using \link{data}.
 #' @param logindata Name of the login data object to be loaded using \link{data}.
-#' @param pkgs The package(s) to look in for datasets, default is all.
-#' @param dslite.server Symbol name to which the \link{DSLiteServer} should be assigned to.
+#' @param pkgs The package(s) to look in for datasets, default is all, then the 'data' subdirectory (if present)
+#' of the current working directory (same behavior as 'package' argument in \link{data}).
+#' @param dslite.server Symbol name to which the \link{DSLiteServer} should be assigned to. If not provided, the symbol name
+#' will be the first not null one specified in the 'url' column of the loaded login data.
 #' @param env The environment where DataSHIELD objects should be looked for: the \link{DSLiteServer} and
 #' the DSIConnection objects. Default is the Global environment.
 #' @return The login data for the \link{datashield.login} function.
 #' @family setup functions
 #' @export
-setupDSLiteServer <- function(packages, datasets, logindata, pkgs = NULL, dslite.server="dslite.server", env = globalenv()) {
+setupDSLiteServer <- function(packages = c(), datasets, logindata, pkgs = NULL, dslite.server = NULL, env = globalenv()) {
   # check server-side package is installed
   for (package in packages) {
     if (!base::requireNamespace(package, quietly = TRUE)) {
@@ -33,6 +35,14 @@ setupDSLiteServer <- function(packages, datasets, logindata, pkgs = NULL, dslite
   for (dataset in datasets) {
     tables[[dataset]] <- get(dataset, envir = env)
   }
+
+  # get server symbol from login data if not provided by argument
+  dslite.server.sym <- dslite.server
+  if (is.null(dslite.server)) {
+    ld <- get(logindata, envir = env)
+    dslite.server.sym <- as.character(ld[!is.null(ld$url),][1,]$url)
+  }
+
   assign(dslite.server, newDSLiteServer(tables=tables), envir = env)
   # specify in which environment the dslite server lives
   options(datashield.env=env)
