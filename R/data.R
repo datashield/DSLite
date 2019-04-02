1d326beff5af41ab36469ed2fab20aff06c02263
Yannick Marcon <yannick.marcon@obiba.org>
2019-04-02 22:22:39 +0200

    SURVIVAL (expand with missing) datasets added

--- R/data.R
+++ R/data.R
@@ -1,34 +1,55 @@
-#' Example login data
-#'
-#' @name logindata.dslite.demo
-#' @docType data
-#' @keywords data
-NULL
-
-#' Example login data for the simulated datasets
+#' Login data for the simulated datasets
 #'
 #' @name logindata.dslite.cnsim
 #' @docType data
 #' @keywords data
 NULL
 
-#' Example simulated dataset 1
+#' Simulated dataset 1
 #'
 #' @name CNSIM1
 #' @docType data
 #' @keywords data
 NULL
 
-#' Example simulated dataset 2
+#' Simulated dataset 2
 #'
 #' @name CNSIM2
 #' @docType data
 #' @keywords data
 NULL
 
-#' Example simulated dataset 3
+#' Simulated dataset 3
 #'
 #' @name CNSIM3
+#' @docType data
+#' @keywords data
+NULL
+
+#' Login data for the simulated survival datasets
+#'
+#' @name logindata.dslite.survival.expand_with_missing
+#' @docType data
+#' @keywords data
+NULL
+
+#' Simulated survival dataset 1
+#'
+#' @name SURVIVAL.EXPAND_WITH_MISSING1
+#' @docType data
+#' @keywords data
+NULL
+
+#' Simulated survival dataset 2
+#'
+#' @name SURVIVAL.EXPAND_WITH_MISSING2
+#' @docType data
+#' @keywords data
+NULL
+
+#' Simulated survival dataset 3
+#'
+#' @name SURVIVAL.EXPAND_WITH_MISSING3
 #' @docType data
 #' @keywords data
 NULL
---
+++ R/setupSURVIVALTest.R
@@ -0,0 +1,17 @@
+#' Setup a test environment based on the SURVIVAL (EXPAND_WITH_MISSING) simulated datasets
+#'
+#' Load the SURVIVAL (EXPAND_WITH_MISSING) datasets, the corresponding login data object, instanciate a new \link{DSLiteServer}
+#' hosting these datasets and verify that the required DataSHIELD server-side packages are installed.
+#'
+#' @param packages DataSHIELD server-side packages which local installation must be verified so that the \link{DSLiteServer}
+#' can auto-configure itself and can execute the DataSHIELD operations. Default is none.
+#' @param env The environment where DataSHIELD objects should be looked for: the \link{DSLiteServer} and
+#' the DSIConnection objects. Default is the Global environment.
+#' @return The login data for the \link{datashield.login} function.
+#' @family setup functions
+#' @export
+setupSURVIVALTest <- function(packages = c(), env = globalenv()) {
+  setupDSLiteServer(packages, c("SURVIVAL.EXPAND_WITH_MISSING1", "SURVIVAL.EXPAND_WITH_MISSING2", "SURVIVAL.EXPAND_WITH_MISSING3"),
+                    "logindata.dslite.survival.expand_with_missing", "DSLite", "dslite.server", env)
+}
+
