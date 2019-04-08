f225d8a0c8750e56a65a67bf48b07c3618b97bb4
Yannick Marcon <yannick.marcon@obiba.org>
2019-04-08 11:13:59 +0200

    Ensure connection objects are of class DSLiteConnection

--- R/getDSLiteData.R
+++ R/getDSLiteData.R
@@ -3,7 +3,7 @@
 #' Get the data value corresponding to the variable with the symbol name from the \link{DSLiteServer} associated
 #' to the \code{\link{DSConnection-class}} object(s). Can be useful when developping a DataSHIELD package.
 #'
-#' @param conns \code{\link{DSConnection-class}} object or a list of \code{\link{DSConnection-class}}s.
+#' @param conns \code{\link{DSConnection-class}} object or a list of \code{\link{DSLiteConnection-class}}s.
 #' @param symbol Symbol name identifying the variable in the \link{DSLiteServer}'s "server-side" environment(s).
 #' @return The data value or a list of values depending on the connections parameter.
 #' @export
@@ -11,6 +11,10 @@ getDSLiteData <- function(conns, symbol) {
   if (is.list(conns)) {
     lapply(conns, function(conn) getDSLiteData(conn, symbol))
   } else {
-    conns@server$getSessionData(conns@sid, symbol)
+    if ("DSLiteConnection" %in% class(conns)) {
+      conns@server$getSessionData(conns@sid, symbol)
+    } else {
+      NA
+    }
   }
 }
