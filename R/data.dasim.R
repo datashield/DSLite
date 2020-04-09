e6b88ce7955c9a66b9cfb873a9624c0cc568a2ba
Yannick Marcon <yannick.marcon@obiba.org>
2020-04-09 14:09:18 +0200

    More dataset doc

--- R/data.dasim.R
+++ R/data.dasim.R
@@ -1,6 +1,19 @@
 #' DataSHIELD login data for the DASIM simulated datasets
 #'
-#' DataSHIELD login data.frame for connecting with DASIM datasets.
+#' DataSHIELD login data.frame for connecting with DASIM datasets. The DASIM datasets contain
+#' synthetic data based on a model derived from the participants of the 1958 Birth Cohort, as
+#' part of the obesity methodological development project. These datasets do not contain some
+#' NA values.
+#'
+#' | **Field**          | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | server             | Server/study name                | char     ||
+#' | url                | Server/study URL                 | char     | DSLiteServer instance symbol name |
+#' | user               | User name                        | char     | Always empty for DSLiteServer |
+#' | password           | User password                    | char     | Always empty for DSLiteServer |
+#' | table              | Table unique name                | char     | As registered in the DSLiteServer |
+#' | options            | Connection options               | char     | Always empty for DSLiteServer |
+#' | driver             | Connection driver                | char     | DSLiteServer |
 #'
 #' @name logindata.dslite.dasim
 #' @docType data
@@ -9,9 +22,23 @@ NULL
 
 #' Simulated dataset DASIM 1
 #'
-#' Simulated dataset DASIM 1, in a data.frame with 10000 observations of 10 harmonized variables:
-#' LAB_TSC, LAB_TRIG, LAB_HDL, LAB_GLUC_FASTING, PM_BMI_CONTINUOUS, DIS_CVA, DIS_DIAB, DIS_AMI,
-#' GENDER, PM_BMI_CATEGORICAL.
+#' Simulated dataset DASIM 1, in a data.frame with 10000 observations of 10 harmonized variables.
+#' The DASIM dataset contains synthetic data based on a model derived from the participants of
+#' the 1958 Birth Cohort, as part of the obesity methodological development project. This dataset
+#' does not contain some NA values.
+#'
+#' | **Variable**       | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | LAB_TSC            | Total Serum Cholesterol          | numeric  | mmol/L   |
+#' | LAB_TRIG           | Triglycerides                    | numeric  | mmol/L   |
+#' | LAB_HDL            | HDL Cholesterol                  | numeric  | mmol/L   |
+#' | LAB_GLUC_FASTING   | Fasting Glucose                  | numeric  | mmol/L   |
+#' | PM_BMI_CONTINUOUS  | Body Mass Index (continuous)     | numeric  | kg/m2    |
+#' | DIS_CVA            | History of Stroke                | factor   | 0 = Never had stroke, 1 = Has had stroke|
+#' | DIS_DIAB           | History of Diabetes              | factor   | 0 = Never had diabetes, 1 = Has had diabetes|
+#' | DIS_AMI            | History of Myocardial Infarction | factor   | 0 = Never had myocardial infarction, 1 = Has had myocardial infarction|
+#' | GENDER             | Gender                           | factor   | 0 = Female, 1 = Male|
+#' | PM_BMI_CATEGORICAL | Body Mass Index (categorical)    | factor   | 1 = Less than 25 kg/m2, 2 = 25 to 30 kg/m2, 3 = Over 30 kg/m2|
 #'
 #' @name DASIM1
 #' @docType data
@@ -20,9 +47,23 @@ NULL
 
 #' Simulated dataset DASIM 2
 #'
-#' Simulated dataset DASIM 2, in a data.frame with 10000 observations of 10 harmonized variables:
-#' LAB_TSC, LAB_TRIG, LAB_HDL, LAB_GLUC_FASTING, PM_BMI_CONTINUOUS, DIS_CVA, DIS_DIAB, DIS_AMI,
-#' GENDER, PM_BMI_CATEGORICAL.
+#' Simulated dataset DASIM 2, in a data.frame with 10000 observations of 10 harmonized variables.
+#' The DASIM dataset contains synthetic data based on a model derived from the participants of
+#' the 1958 Birth Cohort, as part of the obesity methodological development project. This dataset
+#' does not contain some NA values.
+#'
+#' | **Variable**       | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | LAB_TSC            | Total Serum Cholesterol          | numeric  | mmol/L   |
+#' | LAB_TRIG           | Triglycerides                    | numeric  | mmol/L   |
+#' | LAB_HDL            | HDL Cholesterol                  | numeric  | mmol/L   |
+#' | LAB_GLUC_FASTING   | Fasting Glucose                  | numeric  | mmol/L   |
+#' | PM_BMI_CONTINUOUS  | Body Mass Index (continuous)     | numeric  | kg/m2    |
+#' | DIS_CVA            | History of Stroke                | factor   | 0 = Never had stroke, 1 = Has had stroke|
+#' | DIS_DIAB           | History of Diabetes              | factor   | 0 = Never had diabetes, 1 = Has had diabetes|
+#' | DIS_AMI            | History of Myocardial Infarction | factor   | 0 = Never had myocardial infarction, 1 = Has had myocardial infarction|
+#' | GENDER             | Gender                           | factor   | 0 = Female, 1 = Male|
+#' | PM_BMI_CATEGORICAL | Body Mass Index (categorical)    | factor   | 1 = Less than 25 kg/m2, 2 = 25 to 30 kg/m2, 3 = Over 30 kg/m2|
 #'
 #' @name DASIM2
 #' @docType data
@@ -31,9 +72,23 @@ NULL
 
 #' Simulated dataset DASIM 3
 #'
-#' Simulated dataset DASIM 3, in a data.frame with 10000 observations of 10 harmonized variables:
-#' LAB_TSC, LAB_TRIG, LAB_HDL, LAB_GLUC_FASTING, PM_BMI_CONTINUOUS, DIS_CVA, DIS_DIAB, DIS_AMI,
-#' GENDER, PM_BMI_CATEGORICAL.
+#' Simulated dataset DASIM 3, in a data.frame with 10000 observations of 10 harmonized variables.
+#' The DASIM dataset contains synthetic data based on a model derived from the participants of
+#' the 1958 Birth Cohort, as part of the obesity methodological development project. This dataset
+#' does not contain some NA values.
+#'
+#' | **Variable**       | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | LAB_TSC            | Total Serum Cholesterol          | numeric  | mmol/L   |
+#' | LAB_TRIG           | Triglycerides                    | numeric  | mmol/L   |
+#' | LAB_HDL            | HDL Cholesterol                  | numeric  | mmol/L   |
+#' | LAB_GLUC_FASTING   | Fasting Glucose                  | numeric  | mmol/L   |
+#' | PM_BMI_CONTINUOUS  | Body Mass Index (continuous)     | numeric  | kg/m2    |
+#' | DIS_CVA            | History of Stroke                | factor   | 0 = Never had stroke, 1 = Has had stroke|
+#' | DIS_DIAB           | History of Diabetes              | factor   | 0 = Never had diabetes, 1 = Has had diabetes|
+#' | DIS_AMI            | History of Myocardial Infarction | factor   | 0 = Never had myocardial infarction, 1 = Has had myocardial infarction|
+#' | GENDER             | Gender                           | factor   | 0 = Female, 1 = Male|
+#' | PM_BMI_CATEGORICAL | Body Mass Index (categorical)    | factor   | 1 = Less than 25 kg/m2, 2 = 25 to 30 kg/m2, 3 = Over 30 kg/m2|
 #'
 #' @name DASIM3
 #' @docType data
--- R/data.survival.R
+++ R/data.survival.R
@@ -1,6 +1,17 @@
 #' DataSHIELD login data for the simulated survival expand-with-missing datasets
 #'
 #' DataSHIELD login data.frame for connecting with SURVIVAL datasets which purpose is to perform survival tests.
+#' The datasets contain synthetic data based on a simulated survival model, including a censoring indicator.
+#'
+#' | **Field**          | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | server             | Server/study name                | char     ||
+#' | url                | Server/study URL                 | char     | DSLiteServer instance symbol name |
+#' | user               | User name                        | char     | Always empty for DSLiteServer |
+#' | password           | User password                    | char     | Always empty for DSLiteServer |
+#' | table              | Table unique name                | char     | As registered in the DSLiteServer |
+#' | options            | Connection options               | char     | Always empty for DSLiteServer |
+#' | driver             | Connection driver                | char     | DSLiteServer |
 #'
 #' @name logindata.dslite.survival.expand_with_missing
 #' @docType data
@@ -9,8 +20,23 @@ NULL
 
 #' Simulated survival expand-with-missing dataset 1
 #'
-#' Simulated dataset SURVIVAL.EXPAND_WITH_MISSING 1, in a data.frame with 2060 observations of 12 harmonized variables:
-#' id, study.id, time.id, starttime, endtime, survtime, cens, age.60, female, noise.56, pm10.16, bmi.26.
+#' Simulated dataset SURVIVAL.EXPAND_WITH_MISSING 1, in a data.frame with 2060 observations of 12 harmonized variables.
+#' The dataset contains synthetic data based on a simulated survival model, including a censoring indicator.
+#'
+#' | **Variable**       | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | id                 | Unique individual ID             | integer  ||
+#' | study.id           | Study ID                         | integer  ||
+#' | time.id            | Time ID                          | integer  ||
+#' | starttime          | Start of follow up               | numeric  | years |
+#' | endtime            | End of follow up                 | numeric  | years |
+#' | survtime           | Survtime                         | numeric  | years |
+#' | cens               | Censoring status                 | factor   | 0 = not censored, 1 = censored |
+#' | age.60             | Age centred at 60                | numeric  ||
+#' | female             | Gender                           | factor   | 0 = Male, 1 = Female |
+#' | noise.56           | Noise pollution centred at 56    | numeric  | dB |
+#' | pm10.16            | Particulate matter centred at 16 | numeric  | μg/m3 |
+#' | bmi.26             | Body mass index centred at 26    | numeric  | kg/m2 |
 #'
 #' @name SURVIVAL.EXPAND_WITH_MISSING1
 #' @docType data
@@ -19,8 +45,23 @@ NULL
 
 #' Simulated survival expand-with-missing dataset 2
 #'
-#' Simulated dataset SURVIVAL.EXPAND_WITH_MISSING 2, in a data.frame with 1640 observations of 12 harmonized variables:
-#' id, study.id, time.id, starttime, endtime, survtime, cens, age.60, female, noise.56, pm10.16, bmi.26.
+#' Simulated dataset SURVIVAL.EXPAND_WITH_MISSING 2, in a data.frame with 1640 observations of 12 harmonized variables.
+#' The dataset contains synthetic data based on a simulated survival model, including a censoring indicator.
+#'
+#' | **Variable**       | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | id                 | Unique individual ID             | integer  ||
+#' | study.id           | Study ID                         | integer  ||
+#' | time.id            | Time ID                          | integer  ||
+#' | starttime          | Start of follow up               | numeric  | years |
+#' | endtime            | End of follow up                 | numeric  | years |
+#' | survtime           | Survtime                         | numeric  | years |
+#' | cens               | Censoring status                 | factor   | 0 = not censored, 1 = censored |
+#' | age.60             | Age centred at 60                | numeric  ||
+#' | female             | Gender                           | factor   | 0 = Male, 1 = Female |
+#' | noise.56           | Noise pollution centred at 56    | numeric  | dB |
+#' | pm10.16            | Particulate matter centred at 16 | numeric  | μg/m3 |
+#' | bmi.26             | Body mass index centred at 26    | numeric  | kg/m2 |
 #'
 #' @name SURVIVAL.EXPAND_WITH_MISSING2
 #' @docType data
@@ -29,8 +70,23 @@ NULL
 
 #' Simulated survival expand-with-missing dataset 3
 #'
-#' Simulated dataset SURVIVAL.EXPAND_WITH_MISSING 3, in a data.frame with 2688 observations of 12 harmonized variables:
-#' id, study.id, time.id, starttime, endtime, survtime, cens, age.60, female, noise.56, pm10.16, bmi.26.
+#' Simulated dataset SURVIVAL.EXPAND_WITH_MISSING 3, in a data.frame with 2688 observations of 12 harmonized variables.
+#' The dataset contains synthetic data based on a simulated survival model, including a censoring indicator.
+#'
+#' | **Variable**       | **Description**                  | **Type** | **Note** |
+#' | ------------------ | -------------------------------- | -------- | -------- |
+#' | id                 | Unique individual ID             | integer  ||
+#' | study.id           | Study ID                         | integer  ||
+#' | time.id            | Time ID                          | integer  ||
+#' | starttime          | Start of follow up               | numeric  | years |
+#' | endtime            | End of follow up                 | numeric  | years |
+#' | survtime           | Survtime                         | numeric  | years |
+#' | cens               | Censoring status                 | factor   | 0 = not censored, 1 = censored |
+#' | age.60             | Age centred at 60                | numeric  ||
+#' | female             | Gender                           | factor   | 0 = Male, 1 = Female |
+#' | noise.56           | Noise pollution centred at 56    | numeric  | dB |
+#' | pm10.16            | Particulate matter centred at 16 | numeric  | μg/m3 |
+#' | bmi.26             | Body mass index centred at 26    | numeric  | kg/m2 |
 #'
 #' @name SURVIVAL.EXPAND_WITH_MISSING3
 #' @docType data
